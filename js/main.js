0a47610eed9794bbf1a08dda8346a96722ca5cba
Nikita <fidg37@gmail.com>
2019-09-24 21:43:18 +0300

    Добавляет функцию, которая добавляет метки объявлений в DOM

--- js/main.js
+++ js/main.js
@@ -1,36 +1,14 @@
 'use strict'
 var arraysLength = 8;
+var map = document.querySelector('.map');
+var pinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');;
+var pinsList = map.querySelector('.map__pins');
+var pinsFragment = document.createDocumentFragment();
 
 var authors = [];
 var offers = [];
 var locationsArray = [];
 var notices = [];
-/*
-{
-  "author": {
-    "avatar": строка, адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём. Например, 01, 02 и т. д. Адреса изображений не повторяются
-  },
-
-  "offer": {
-    "title": строка, заголовок предложения
-    "address": строка, адрес предложения. Для простоты пусть пока представляет собой запись вида "{{location.x}}, {{location.y}}", например, "600, 350"
-    "price": число, стоимость
-    "type": строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
-    "rooms": число, количество комнат
-    "guests": число, количество гостей, которое можно разместить
-    "checkin": строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,
-    "checkout": строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
-    "features": массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",
-    "description": строка с описанием,
-    "photos": массив строк случайной длины, содержащий адреса фотографий "http://o0.github.io/assets/images/tokyo/hotel1.jpg", "http://o0.github.io/assets/images/tokyo/hotel2.jpg", "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-  },
-
-  "location": {
-    "x": случайное число, координата x метки на карте. Значение ограничено размерами блока, в котором перетаскивается метка.
-    "y": случайное число, координата y метки на карте от 130 до 630.
-  }
-}
-*/
 
 var getRandom = function (min, max) {
   min = Math.ceil(min);
@@ -177,13 +155,17 @@ var createOffersArray = function () {
 var createLocationsArray = function () {
   for (var i = 0; i < arraysLength; i++) {
     locationsArray[i] = {
-      x: getRandom(0, 400),
+      x: getRandom(0, 1000),
       y: getRandom(130, 630)
     }
   };
 };
 
 var createNoticeArray = function () {
+  createAuthorsArray();
+  createOffersArray();
+  createLocationsArray();
+
   for (var i = 0; i < arraysLength; i++) {
     notices[i] = {
       author: authors[i]
@@ -193,12 +175,31 @@ var createNoticeArray = function () {
   };
 };
 
-createAuthorsArray();
-createOffersArray();
-createLocationsArray();
-createNoticeArray()
+var createPin = function (object) {
+  var pin = pinTemplate.cloneNode(true);
+  pin.style.left = object.location.x + 'px';
+  pin.style.top = object.location.y + 'px';
+  pin.querySelector('img').src = object.author.avatar;
+  pin.querySelector('img').alt = object.offer.title;
+
+  return pin;
+}
+
+var createPinsList = function() {
+  for (var i = 0; i < arraysLength; i++) {
+    pinsFragment.appendChild(createPin(notices[i]));
+  };
+  pinsList.appendChild(pinsFragment);
+};
+
+createNoticeArray();
+createPinsList();
+
+var pin = pinTemplate.cloneNode(true);
 
 console.log(authors);
 console.log(offers);
 console.log(locationsArray);
 console.log(notices);
+
+map.classList.remove('map--faded');
